932c9fac5c964cb7ef6d155464dc8c269089c326
SirajArf <96326682+SirajArf@users.noreply.github.com>
2024-06-12 11:39:57 +0530

    Initial commit

---
+++ allocation/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import Asset from './components/Asset'
+
+function App() {
+  return (
+    <div className="App">
+      <Asset/>
+    </div>
+  );
+}
+
+export default App;
---
+++ allocation/src/components/Asset.js
@@ -0,0 +1,172 @@
+import React from "react";
+import "../css/Asset.css";
+
+const Asset = () => {
+  const handleClick = (e) => {
+    // e.preventDefault();
+    console.log("clicked");
+  };
+  const handleSubmit = (e) => {
+    // e.preventDefault();
+    console.log("submit");
+  };
+
+  return (
+    <div>
+      <h1 className="head">Asset Handover Form</h1>
+
+      <h3>Handover to</h3>
+      <hr />
+
+      <form>
+        <div>
+          <label htmlFor="name">Name</label>
+          <br />
+          <input
+            type="text"
+            className="p-1"
+            placeholder="Enter your first name"
+          />
+
+          <input
+            type="text"
+            className="p-1 m-3"
+            placeholder="Enter your last name"
+          />
+        </div>
+        <div>
+          <label htmlFor="name">Email</label>
+          <input type="email" className="m-3" placeholder="Enter your email" />
+        </div>
+        <div className="d-inline-flex p-2 bd-highlight">
+          <h3>Employee ID</h3>
+          <input
+            type="text"
+            className="m-3"
+            placeholder="Enter your employee ID"
+          ></input>
+        </div>
+        <div>
+          <h3>Asset Description</h3>
+        </div>
+
+        <div className="d-inline-flex bd-highlight">
+          <div>
+            <label htmlFor="asset" className="mr-3">
+              Asset
+            </label>
+            <br />
+            <input
+              type="text"
+              className="mt-3"
+              placeholder="Enter asset"
+            ></input>
+          </div>
+          <div>
+            <label htmlFor="Sumber">Serial Number</label>
+            <br />
+            <input
+              type="number"
+              className="m-3"
+              placeholder="Enter serial number"
+            ></input>
+          </div>
+          <div>
+            <label htmlFor="quantity">Quantity</label>
+            <br />
+            <input type="number" className="m-3"></input>
+          </div>
+          <div>
+            <label htmlFor="accessories">Accessories</label>
+            <br />
+            <input type="text" className="m-3"></input>
+          </div>
+        </div>
+
+        <br />
+        <button
+          type="button"
+          className="btn btn-light m-3 "
+          onClick={handleClick}
+        >
+          + Add Row
+        </button>
+      </form>
+
+      <form></form>
+      <div>
+        <h3>Handovered By</h3>
+        <hr />
+        <label htmlFor="name" className="d-inline-flex p-2 bd-highlight">
+          Name
+        </label>
+        <br />
+        <input
+          type="text"
+          className="m-3"
+          placeholder="Enter your first name"
+        />
+        <input type="text" className="m-3" placeholder="Enter your last name" />
+      </div>
+      <div>
+        <label htmlFor="name">Email</label>
+        <input
+          type="email"
+          className="m-3"
+          id="email"
+          placeholder="Enter your email"
+        />
+      </div>
+      <div>
+        <label htmlFor="empid">Employee ID</label>
+        <input
+          type="text"
+          className="m-3"
+          placeholder="Enter your employee ID"
+        ></input>
+      </div>
+      <div>
+        <label htmlFor="dh">Date of handover</label>
+        <input
+          type="date"
+          className="m-3"
+          placeholder="Enter your employee ID"
+        ></input>
+      </div>
+      <div>
+        <h1>Acknowledgment and Declaration by the Employee</h1>
+        <hr />
+        <ol>
+          <li>
+            I hereby acknowledge that I have received the above mentioned
+            assets. I understand that this asset belong to and is my under
+            possession for carrying out my office work. I hereby assure that I
+            own the responsibility to care of the asset of the company to its
+            fullest extend. On resignation/termination, I will immediateley
+            handover this asset and its working condition to the company
+          </li>
+          <li>
+            I will be soleby responsible for the company belonging with me.{" "}
+          </li>
+        </ol>
+      </div>
+      <div>
+        <label htmlFor="signature" className="d-inline-flex bd-highlight">
+          Signature of Employee
+        </label>
+        <input type="text" className="m-3"></input>
+        <button>clear</button>
+      </div>
+      <hr />
+      <button
+        type="button"
+        className="btn btn-primary m-3 "
+        onClick={handleSubmit}
+      >
+        Submit
+      </button>
+    </div>
+  );
+};
+
+export default Asset;
